fba0710e02c8aaec9465e846f3067fc64ba33573
SakuraChiyo0v0 <3296299414@qq.com>
2024-12-02 03:48:12 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import './assets/base.scss'
+
+//app
+import { createApp } from 'vue'
+import App from './App.vue'
+const app = createApp(App)
+
+//elementIcon库
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+//router
+import router from './router'
+app.use(router)
+
+//pinia
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate' //引入持久化插件
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
+
+
+app.mount('#app')
